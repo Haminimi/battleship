dc50586532e761ab7e67577e2097cda3bad877dc
Hamza Mujinovic <mujinovichamza@gmail.com>
2024-01-03 20:15:32 +0100

    Add a test for the Player class

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -3,6 +3,7 @@
 import { sum } from './index';
 import { Ship } from './ship';
 import { Gameboard } from './gameboard';
+import { Player } from './player';
 
 test('sum', () => {
 	expect(sum(1, 2)).toBe(3);
@@ -36,4 +37,29 @@ test('Gameboard', () => {
 	expect(gameboard.ships[0].numberOfHits).toBe(5);
 	expect(gameboard.ships[0].sunk).toBeTruthy();
 	expect(gameboard.gameOver()).toBeTruthy();
+});
+
+test('Player', () => {
+	const gameboard = new Gameboard();
+	const enemyGameboard = new Gameboard();
+	gameboard.initializeCoordinates();
+	enemyGameboard.initializeCoordinates();
+	const player = new Player('human', gameboard);
+	const computer = new Player('computer', enemyGameboard);
+	const ship = new Ship(5);
+	const enemyShip = new Ship(5);
+	gameboard.placeShip(ship, ['0, 0', '0, 1', '0, 2', '0, 3', '0, 4']);
+	enemyGameboard.placeShip(enemyShip, [
+		'0, 0',
+		'0, 1',
+		'0, 2',
+		'0, 3',
+		'0, 4',
+	]);
+	expect(player.gameboard).toBe(gameboard);
+	expect(player.gameboard.coordinates['0, 0']).toBe(ship);
+	expect(computer.name).toBe('computer');
+	expect(computer.previousMoves.length).toBe(0);
+	computer.computerMove(enemyGameboard);
+	expect(computer.previousMoves.length).toBe(1);
 });
