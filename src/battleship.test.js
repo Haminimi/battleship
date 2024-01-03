8c7d551ad64bb1032a690b70ae5861e976fcd456
Hamza Mujinovic <mujinovichamza@gmail.com>
2024-01-03 19:16:31 +0100

    Add a test for the Gameboard class

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -2,6 +2,7 @@
 
 import { sum } from './index';
 import { Ship } from './ship';
+import { Gameboard } from './gameboard';
 
 test('sum', () => {
 	expect(sum(1, 2)).toBe(3);
@@ -19,4 +20,20 @@ test('Ship', () => {
 	ship.hit();
 	ship.hit();
 	expect(ship.isSunk()).toBe(true);
+});
+
+test('Gameboard', () => {
+	const gameboard = new Gameboard();
+	gameboard.initializeCoordinates();
+	const ship = new Ship(5);
+	gameboard.placeShip(ship, ['0, 0', '0, 1', '0, 2', '0, 3', '0, 4']);
+	gameboard.receiveAttack('0, 0');
+	gameboard.receiveAttack('0, 1');
+	gameboard.receiveAttack('0, 2');
+	gameboard.receiveAttack('0, 3');
+	gameboard.receiveAttack('0, 4');
+	expect(gameboard.ships[0].length).toBe(5);
+	expect(gameboard.ships[0].numberOfHits).toBe(5);
+	expect(gameboard.ships[0].sunk).toBeTruthy();
+	expect(gameboard.gameOver()).toBeTruthy();
 });
