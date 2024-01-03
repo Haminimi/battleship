a7a2d51aa681f761bfed0bc81303eaf4f55fe059
Hamza Mujinovic <mujinovichamza@gmail.com>
2024-01-03 20:16:16 +0100

    Add the Player class

---
+++ src/player.js
@@ -0,0 +1,19 @@
+export class Player {
+	constructor(name, gameboard) {
+		this.name = name;
+		this.gameboard = gameboard;
+		this.previousMoves = [];
+	}
+
+	computerMove(enemyGameboard) {
+		const firstRandomNumber = Math.floor(Math.random() * (10 - 0));
+		const secondRandomNumber = Math.floor(Math.random() * (10 - 0));
+		const move = `${firstRandomNumber}, ${secondRandomNumber}`;
+		if (!this.previousMoves.includes(move)) {
+			enemyGameboard.receiveAttack(move);
+			this.previousMoves.push(move);
+		} else {
+			this.computerMove(enemyGameboard);
+		}
+	}
+}
