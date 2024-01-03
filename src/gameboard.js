8d870ab260c077e9280ea729cfd212e31651048f
Hamza Mujinovic <mujinovichamza@gmail.com>
2024-01-03 19:16:58 +0100

    Add the Gameboard class

---
+++ src/gameboard.js
@@ -0,0 +1,40 @@
+/* eslint-disable */
+
+export class Gameboard {
+	constructor() {
+		this.coordinates = {};
+		this.missed = [];
+		this.ships = [];
+	}
+
+	initializeCoordinates() {
+		for (let i = 0; i < 10; i++) {
+			for (let j = 0; j < 10; j++) {
+				this.coordinates[`${i}, ${j}`] = true;
+			}
+		}
+	}
+
+	placeShip(ship, coordinates) {
+		const shipLength = ship.length;
+		if (shipLength === coordinates.length) {
+			this.ships.push(ship);
+			coordinates.forEach((coordinate) => {
+				this.coordinates[coordinate] = ship;
+			});
+		}
+	}
+
+	receiveAttack(coordinates) {
+		if (this.coordinates[coordinates] !== true) {
+			const ship = this.coordinates[coordinates];
+			ship.hit();
+		} else {
+			this.missed.push(coordinates);
+		}
+	}
+
+	gameOver() {
+		return this.ships.every((ship) => ship.sunk === true);
+	}
+}
