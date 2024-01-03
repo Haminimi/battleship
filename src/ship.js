c9a97dfb092436d672b8158d9c9113e645ecaf62
Hamza Mujinovic <mujinovichamza@gmail.com>
2024-01-03 20:18:42 +0100

    Add the logic to determine if a ship is sunk

--- src/ship.js
+++ src/ship.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 export class Ship {
 	constructor(length, numberOfHits = 0, sunk = false) {
 		this.length = length;
@@ -7,9 +9,12 @@ export class Ship {
 
 	hit() {
 		this.numberOfHits += 1;
+		if (this.numberOfHits >= this.length) {
+			this.sunk = true;
+		}
 	}
 
 	isSunk() {
-		return this.numberOfHits >= this.length;
+		return this.sunk;
 	}
 }
