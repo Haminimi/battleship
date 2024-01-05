e814d77caef9108ba1cf19f60d2810fb72c18375
Hamza Mujinovic <mujinovichamza@gmail.com>
2024-01-06 00:42:25 +0100

    Add the imports

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './style.css';
+import { Player } from './player';
+import { Gameboard } from './gameboard';
+import { Ship } from './ship';
+import render, {
+	showAttack,
+	showComputerAttack,
+	showNextDragShip,
+} from './render';
